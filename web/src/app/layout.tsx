8d948b6205df12f280ad3d5184cd1113a7a474c8
Bruno Vinícius <vbruno96@gmail.com>
2023-05-18 20:05:34 -0300

    create layout application

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,9 +1,16 @@
 import { ReactNode } from 'react'
-import './globals.css'
 import {
   Bai_Jamjuree as BaiJamjuree,
   Roboto_Flex as Roboto,
 } from 'next/font/google'
+import { cookies } from 'next/headers'
+
+import { Copyright } from '@/app/components/Copyright'
+import { Hero } from '@/app/components/Hero'
+import { Profile } from '@/app/components/Profile'
+import { SignIn } from '@/app/components/SignIn'
+
+import './globals.css'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const baiJamjuree = BaiJamjuree({
@@ -19,12 +26,29 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-12">
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+            {/* Stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
+          {/* Right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
