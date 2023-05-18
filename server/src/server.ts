0be57c0d4b606aef48c6aba28433d757c4daa4fd
Bruno Vinícius <vbruno96@gmail.com>
2023-05-18 11:19:33 -0300

    create register route

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,9 @@ import 'dotenv/config'
 
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -10,7 +12,12 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: 'spacetime_nlw_sdfvjklhnbjkldvfjksdfg',
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
