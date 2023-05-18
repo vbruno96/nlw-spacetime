d336fe76dd2dc6bbefd3b17ce1e67fc3c9b9f7d4
Bruno Vinícius <vbruno96@gmail.com>
2023-05-18 10:24:48 -0300

    create hero component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,6 @@
 import { User } from 'lucide-react'
-import Image from 'next/image'
-import nlwLogo from '../assets/nlw-spacetime-logo.svg'
-import { Copyright } from './components/Copyright'
+import { Copyright } from '@/app/components/Copyright'
+import { Hero } from '@/app/components/Hero'
 
 export default function Home() {
   return (
@@ -27,26 +26,7 @@ export default function Home() {
         </a>
 
         {/* Hero */}
-        <div className="space-y-5">
-          <Image src={nlwLogo} alt="NLW Spacetime" />
-
-          <div className="max-w-[420px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-
-          <a
-            href=""
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-title text-sm uppercase leading-none text-black hover:bg-green-600"
-          >
-            CADASTRAR LEMBRANÇA
-          </a>
-        </div>
+        <Hero />
 
         {/* Copyright */}
         <Copyright />
