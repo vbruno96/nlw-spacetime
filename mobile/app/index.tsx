85a176046d1c077b9d8e1da0c7adba92ac7b32c9
Bruno Vinícius <vbruno96@gmail.com>
2023-05-18 15:49:21 -0300

    fix layout on ios

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,5 +1,11 @@
 import { useEffect } from 'react'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import {
+  ImageBackground,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 
 import { styled } from 'nativewind'
 
@@ -66,45 +72,47 @@ export default function App() {
   if (!hasLoadedFonts) return null
 
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-zinc-900 px-8 py-10"
-      imageStyle={{
-        position: 'absolute',
-        left: '-100%',
-      }}
-    >
-      <StyledStripes className="absolute left-2" />
+    <SafeAreaView className="flex-1 bg-zinc-900">
+      <ImageBackground
+        source={blurBg}
+        className={`relative flex-1 items-center bg-zinc-900 px-8`}
+        imageStyle={{
+          position: 'absolute',
+          left: '-100%',
+        }}
+      >
+        <StyledStripes className="absolute left-2" />
 
-      <View className="flex-1 items-center justify-center gap-6">
-        <NLWLogo />
+        <View className="flex-1 items-center justify-center gap-6">
+          <NLWLogo />
 
-        <View className="space-y-2">
-          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
-            Sua cápsula do tempo
-          </Text>
-          <Text className="text-body text-center text-base leading-relaxed text-gray-100">
-            Colecione momentos marcantes da sua jornada e compartilhe (se
-            quiser) com o mundo!
-          </Text>
+          <View className="space-y-2">
+            <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </Text>
+            <Text className="text-body text-center text-base leading-relaxed text-gray-100">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </Text>
+          </View>
+
+          <TouchableOpacity
+            activeOpacity={0.7}
+            className="rounded-full bg-green-500 px-5 py-3"
+            onPress={() => signInWithGithub()}
+          >
+            <Text className="font-alt text-sm uppercase text-black">
+              Começar a cadastrar
+            </Text>
+          </TouchableOpacity>
         </View>
 
-        <TouchableOpacity
-          activeOpacity={0.7}
-          className="rounded-full bg-green-500 px-5 py-3"
-          onPress={() => signInWithGithub()}
-        >
-          <Text className="font-alt text-sm uppercase text-black">
-            Começar a cadastrar
-          </Text>
-        </TouchableOpacity>
-      </View>
+        <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da Rocketseat
+        </Text>
 
-      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
-        Feito com 💜 no NLW da Rocketseat
-      </Text>
-
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+        <StatusBar style="light" translucent />
+      </ImageBackground>
+    </SafeAreaView>
   )
 }
