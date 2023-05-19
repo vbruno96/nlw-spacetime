096cf540b21147c8713bb0d71f6f371fc142688d
Bruno Vinícius <vbruno96@gmail.com>
2023-05-18 21:24:19 -0300

    create layout for mobile screens

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,62 @@
+import { SafeAreaView, ImageBackground } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import * as SecureStore from 'expo-secure-store'
+import { styled } from 'nativewind'
+
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { SplashScreen, Stack } from 'expo-router'
+import { useEffect, useState } from 'react'
+
+const StyledStripes = styled(Stripes)
+
+export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState(false)
+
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) =>
+      setIsUserAuthenticated(!!token),
+    )
+  }, [])
+
+  if (!hasLoadedFonts) return <SplashScreen />
+
+  return (
+    <SafeAreaView className="flex-1 bg-zinc-900">
+      <ImageBackground
+        source={blurBg}
+        className={`relative flex-1`}
+        imageStyle={{
+          position: 'absolute',
+          left: '-100%',
+        }}
+      >
+        <StyledStripes className="absolute left-2" />
+        <StatusBar style="light" translucent />
+
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: 'transparent' },
+          }}
+        >
+          <Stack.Screen name="index" redirect={isUserAuthenticated} />
+          <Stack.Screen name="memories" />
+        </Stack>
+      </ImageBackground>
+    </SafeAreaView>
+  )
+}
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,32 +1,13 @@
 import { useEffect } from 'react'
-import {
-  ImageBackground,
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native'
-
-import { styled } from 'nativewind'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 import { StatusBar } from 'expo-status-bar'
 import { useRouter } from 'expo-router'
 import * as SecureStore from 'expo-secure-store'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 
 import { api } from '../src/lib/api'
-
-const StyledStripes = styled(Stripes)
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -36,12 +17,6 @@ const discovery = {
 }
 
 export default function App() {
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
-
   const [, response, signInWithGithub] = useAuthRequest(
     {
       clientId: '24cc1bf489db331ad588',
@@ -69,50 +44,37 @@ export default function App() {
     }
   }, [response])
 
-  if (!hasLoadedFonts) return null
-
   return (
-    <SafeAreaView className="flex-1 bg-zinc-900">
-      <ImageBackground
-        source={blurBg}
-        className={`relative flex-1 items-center bg-zinc-900 px-8`}
-        imageStyle={{
-          position: 'absolute',
-          left: '-100%',
-        }}
-      >
-        <StyledStripes className="absolute left-2" />
+    <View className={`relative flex-1 items-center px-8`}>
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
 
-        <View className="flex-1 items-center justify-center gap-6">
-          <NLWLogo />
-
-          <View className="space-y-2">
-            <Text className="text-center font-title text-2xl leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </Text>
-            <Text className="text-body text-center text-base leading-relaxed text-gray-100">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            activeOpacity={0.7}
-            className="rounded-full bg-green-500 px-5 py-3"
-            onPress={() => signInWithGithub()}
-          >
-            <Text className="font-alt text-sm uppercase text-black">
-              Começar a cadastrar
-            </Text>
-          </TouchableOpacity>
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-body text-center text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
         </View>
 
-        <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
-          Feito com 💜 no NLW da Rocketseat
-        </Text>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-3"
+          onPress={() => signInWithGithub()}
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Começar a cadastrar
+          </Text>
+        </TouchableOpacity>
+      </View>
 
-        <StatusBar style="light" translucent />
-      </ImageBackground>
-    </SafeAreaView>
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+
+      <StatusBar style="light" translucent />
+    </View>
   )
 }
